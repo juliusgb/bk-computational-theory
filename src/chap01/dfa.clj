0e1f523a9b0de5f5beb3640ee6776f20c64b82a1
Julius Gamanyi <julius.gb@googlemail.com>
2009-06-02 14:00:59 +0100

    Added maps as return values instead of strings and tested them on 2 automata. Closes gh-4.

--- src/chap01/dfa.clj
+++ src/chap01/dfa.clj
@@ -56,14 +56,16 @@ trans[ition] table."
 
 
 (defn run-dfa-helper [inputs dfa]
-  "Deals with the case when the automata receives an empty string as input."
+  "Deals with all input except the empty input."
   ((fn [state inputs]
      (cond (and (not (contains? (dfa :final-states) state))
                 (empty? inputs))
-             (str "un-acceptable - " state)
+;             (str "un-acceptable - " state)
+             {(keyword "un-acceptable") state}
            (and (contains? (dfa :final-states) state)
                 (empty? inputs))
-             (str "accept - " state)
+;             (str "accept - " state)
+             {(keyword "accept") state}
            :else
              (recur (state-after-transition state
                                             (first inputs)
@@ -73,85 +75,81 @@ trans[ition] table."
 
 
 (defn run-dfa [inputs dfa]
+  "Accepts the empty string as input."
   (let [proc-label (first (dfa :start-state))
         start-state (second (dfa :start-state))]
     (if (and (= "unprocessed" proc-label) (empty? inputs))
-      "accept empty input"
+      {(keyword "accept empty input") start-state}
       (run-dfa-helper inputs (assoc dfa :start-state start-state)))))
 
+;;-------------------------------------------------------------------------
+;; TESTS
+;; TODO: separate out these tests into their own file.
+;;--------------------------------------------------------------------------
 
-
-;; ITC Page 36, Fig 1.6, Finite automata M1
-;; dfa: L(M1) = {w | w contains at least one 1 and
-;;               an even number of 0s follow the last 1}.
-(def m1-alph-list '(0 1))
-(def m1-trans-1 (make-transition "q1" m1-alph-list '("q1" "q2")))
-(def m1-trans-2 (make-transition "q2" m1-alph-list '("q3" "q1")))
-(def m1-trans-3 (make-transition "q3" m1-alph-list '("q2" "q2")))
-(def m1-trans-table (combine-trans m1-trans-1 m1-trans-2 m1-trans-3))
-(def m1-all-states '("q1" "q2" "q3"))
-(def m1-start-state "q1")
-(def m1-fin-state (make-final-states "q2"))
-
-(def m1-dfa (make-dfa m1-all-states m1-alph-list m1-trans-table
-                      m1-start-state m1-fin-state))
 
 (deftest test-m1
-  (is (= "accept empty input"
-         (run-dfa '() m1-dfa))
-      "Empty input should be accepted.")
-  (is (= "un-acceptable - q1"
-         (run-dfa '(0) m1-dfa)))
-  (is (= "accept - q2"
-         (run-dfa '(1) m1-dfa))
-      "String ending with a 1 should be accepted in state q2.")
-  (is (= "accept - q2"
-         (run-dfa '(0 1) m1-dfa))
-      "String ending with a 1 should be accepted in state q2.")
-  (is (= "accept - q2"
-         (run-dfa '(1 1) m1-dfa))
-      "String ending with a 1 should be accepted in state q2.")
-  (is (= "accept - q2"
-         (run-dfa '(1 0 0) m1-dfa)))
-  (is (= "un-acceptable - q1"
-         (run-dfa '(1 1 0) m1-dfa))))
+  "Uses automata M1, Fig 1.6, on page 36. L(M1) = {w | w contains at least
+one 1 or an even number of 0s follow the last 1."
+  (def m1-alph-list '(0 1))
+  (def m1-trans-1 (make-transition "q1" m1-alph-list '("q1" "q2")))
+  (def m1-trans-2 (make-transition "q2" m1-alph-list '("q3" "q2")))
+  (def m1-trans-3 (make-transition "q3" m1-alph-list '("q2" "q2")))
+  (def m1-trans-table (combine-trans m1-trans-1 m1-trans-2 m1-trans-3))
+  (def m1-all-states '("q1" "q2" "q3"))
+  (def m1-start-state "q1")
+  (def m1-fin-state (make-final-states "q2"))
+  (def m1-dfa (make-dfa m1-all-states m1-alph-list m1-trans-table
+                        m1-start-state m1-fin-state))
 
-
-;; ITC page 37, Fig 1.8, M2 - easier.
-;; dfa: L(M2) = {w | w ends in a 1}.
-(def m2-alph-list '(0 1))
-(def m2-trans-1 (make-transition "q1" m2-alph-list '("q1" "q2")))
-(def m2-trans-2 (make-transition "q2" m2-alph-list '("q1" "q2")))
-(def m2-trans-table (combine-trans m2-trans-1 m2-trans-2))
-(def m2-all-states '("q1" "q2"))
-(def m2-start-state "q1")
-(def m2-fin-sate (make-final-states "q2"))
-
-(def m2-dfa (make-dfa m2-all-states m2-alph-list m2-trans-table
-                      m2-start-state m2-fin-sate))
+  (let [empty-state {(keyword "accept empty input") "q1"}
+        accept-state {(keyword "accept") "q2"}
+        accept-msg "String ending with a 1 should be accepted in state q2."
+        un-acceptable-state {(keyword "un-acceptable") "q1"}
+        un-accept-msg "Un-accepted input should be in state q1."
+        un-acceptable-state2 {(keyword "un-acceptable") "q3"}
+        un-accept-msg2 "Un-accepted input should be in state q3."]
+    (is (= empty-state
+           (run-dfa '() m1-dfa))
+        "Empty input should be accepted.")
+    (is (= un-acceptable-state
+           (run-dfa '(0) m1-dfa)))
+    (is (= accept-state (run-dfa '(1) m1-dfa)) accept-msg)
+    (is (= accept-state (run-dfa '(0 1) m1-dfa)) accept-msg)
+    (is (= accept-state (run-dfa '(1 1) m1-dfa)) accept-msg)
+    (is (= accept-state (run-dfa '(1 0 0) m1-dfa)) accept-msg)
+    (is (= un-acceptable-state2 (run-dfa '(1 1 0) m1-dfa)) un-accept-msg2)))
 
 
 (deftest test-m2
-  (is (= "accept empty input"
-         (run-dfa '() m2-dfa))
-      "Empty input should be accepted.")
-  (is (= "un-acceptable - q1"
-         (run-dfa '(0) m2-dfa)))
-  (is (= "accept - q2"
-         (run-dfa '(1) m2-dfa)))
-  (is (= "un-acceptable - q1"
-         (run-dfa '(0 0) m2-dfa)))
-  (is (= "accept - q2"
-         (run-dfa '(1 1) m2-dfa)))
-  (is (= "accept - q2"
-         (run-dfa '(0 0 1) m2-dfa)))
-  (is (= "un-acceptable - q1"
-         (run-dfa '(1 1 0) m2-dfa)))
-  (is (= "accept - q2"
-         (run-dfa '(1 1 0 1) m2-dfa))))
+  "Uses automata M2, Fig 1.8, on page 37. L(M2) = {w | w ends in a 1}."
+  (def m2-alph-list '(0 1))
+  (def m2-trans-1 (make-transition "q1" m2-alph-list '("q1" "q2")))
+  (def m2-trans-2 (make-transition "q2" m2-alph-list '("q1" "q2")))
+  (def m2-trans-table (combine-trans m2-trans-1 m2-trans-2))
+  (def m2-all-states '("q1" "q2"))
+  (def m2-start-state "q1")
+  (def m2-fin-sate (make-final-states "q2"))
+  (def m2-dfa (make-dfa m2-all-states m2-alph-list m2-trans-table
+                        m2-start-state m2-fin-sate))
+
+  (let [empty-state {(keyword "accept empty input") "q1"}
+        accept-state {(keyword "accept") "q2"}
+        accept-msg "Accepted input should be in state q2."
+        un-accept-state {(keyword "un-acceptable") "q1"}
+        un-accept-msg "Un-acceptable input should be in state q1."]
+    (is (= empty-state (run-dfa '() m2-dfa))
+        "Empty input should be accepted")
+    (is (= accept-state (run-dfa '(1) m2-dfa)) accept-msg)
+    (is (= un-accept-state (run-dfa '(0) m2-dfa)) un-accept-msg)
+    (is (= un-accept-state (run-dfa '(0 0) m2-dfa)) un-accept-msg)
+    (is (= accept-state (run-dfa '(1 1) m2-dfa)) accept-msg)
+    (is (= accept-state (run-dfa '(0 0 1) m2-dfa)) accept-msg)
+    (is (= un-accept-state (run-dfa '(1 1 0) m2-dfa)) un-accept-msg)
+    (is (= accept-state (run-dfa '(1 1 0 1) m2-dfa)) accept-msg)))
 
 
-;; Page 38, Example 1.9, Figure 10.
+;; Page 38, Example 1.9, Fig 10.
 ;; L(M3) = {w | w is empty string or ends in a 0}.
 (def m3-alph-list '(0 1))
 (def m3-trans-1 (make-transition "q1" m2-alph-list '("q1" "q2")))
